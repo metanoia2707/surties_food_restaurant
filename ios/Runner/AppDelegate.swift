3faea6d7b55c92f50fa4e8e09e3f6c5b62112882
metanoia-apple <metanoiasolution@gmail.com>
2025-01-03 13:52:03 +0530

    IOS Ready.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-
+import GoogleMaps;
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +8,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyBWi8eR9BJmi6nKpp4eos31rJdGEn-3Tts")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
